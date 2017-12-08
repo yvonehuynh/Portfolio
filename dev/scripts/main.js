790991aa80cd402b7f42e2142470b2704233831b
Yvone Huynh <sw.yvone.huynh@gmail.com>
2017-12-08 16:55:05 -0500

    added flickity function

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,16 +1,8 @@
-
-
-
-    // flickity
-function flickity(){
+$(document).ready(function () {
     $('.main-carousel').flickity({
-        // options
         cellAlign: 'left',
-        contain: true
+        contain: true,
+        imagesLoaded: true
     });
-}
 
-
-$(document).ready({
-    flickity();
 });
