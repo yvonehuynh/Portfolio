32c840fcd014b06bfd6c4b35dab2cbe542644ee2
Yvone Huynh <sw.yvone.huynh@gmail.com>
2017-12-08 15:03:52 -0500

    removed wireframe borders

---
+++ dev/scripts/main.js
@@ -0,0 +1,16 @@
+
+
+
+    // flickity
+function flickity(){
+    $('.main-carousel').flickity({
+        // options
+        cellAlign: 'left',
+        contain: true
+    });
+}
+
+
+$(document).ready({
+    flickity();
+});
