e0d5f7aaaf8a581068cb627da1d4e19fdb4a6cf9
Yvone Huynh <sw.yvone.huynh@gmail.com>
2017-12-09 15:25:40 -0500

    added wow.js functionality

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,5 +1,19 @@
 let script = {};
 
+/* WOW Animationa */
+const wowAnimate=(()=>{
+    new WOW(
+        {
+            boxClass: 'wow',
+            animateClass: 'animated',
+            offset: 100,
+            mobile: false,
+            live: true        // default
+        }
+    ).init();
+    
+})
+
 /* Animate on Scroll */
 function animateScroll(){
         (function ($) {
@@ -65,11 +79,76 @@ function flickity(){
     });
 };
 
+/* TypeWriter Effect */
+
+const animateType=(()=>{
+    var TxtType = function (el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 2) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function () {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+            delta = this.period;
+            this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+            this.isDeleting = false;
+            this.loopNum++;
+            delta = 500;
+        }
+
+        setTimeout(function () {
+            that.tick();
+        }, delta);
+    };
+
+    window.onload = function () {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i = 0; i < elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+                new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 1rem solid #000}";
+        document.body.appendChild(css);
+    };
+    //RERUN
+})
+
 script.init = (()=>{
+    animateType();
     animateScroll();
     flickity();
+    wowAnimate();
 })
 
 $(document).ready(function () {
     script.init();
+    new WOW().init()
  });
